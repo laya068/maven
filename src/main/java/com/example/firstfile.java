bfb3d4332474dee4f0137d08db54be979645de65
MLRIT <madesruthilaya19@example.com>
2024-11-18 21:13:11 +0530

    Added FirstFile.java

---
+++ src/main/java/com/example/firstfile.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class firstfile {
+ public static void main(String[] args)
+ {
+    System.out.println("first  file");
+ }   
+}
---
+++ src/main/java/com/example/secondfile.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class secondfile {
+    public static void main(String[] args)
+    {
+        System.out.println("second file");
+    }
+    
+}
